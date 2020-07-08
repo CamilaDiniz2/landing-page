f398a566ea60da5da684c4a68e711289b4c37dc7
CamilaDiniz2 <camilla.a.diniz@gmail.com>
2020-07-07 22:18:49 -0300

    Atualização do pop-up, limitando para 1, o número de vezes que ele aparece

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const modalOverlay = document.querySelector(".modal-overlay")
 
 var w = screen.width;
 var h = screen.height;
+var count = 0;
+var maxCount = 1;
 
-window.addEventListener ('mouseout', function(e){
-  if (e.x < 0 || e.x > w-300 ||  e.y < 0 || e.y > h){
-    modalOverlay.classList.add('active')
-  }
-}) 
+window.addEventListener('load', (event) => {
+  setInterval(function() {
+    window.addEventListener ('mouseout', function(e){
+      if ((e.x < 0 || e.x > w-300 ||  e.y < 0 || e.y > h) && count < maxCount){
+        modalOverlay.classList.add('active')
+        count += 1
+      }
+    })
+  }, 3000)  
+});
+
+
  
 document.querySelector('.close-modal')
   .addEventListener('click', function(){
     modalOverlay.classList.remove('active')
   }
 )
+
+
